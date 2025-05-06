d55ea5f2bc0e02bf4d6d3e2c74f195db115c334b
Samuel Taggart <sam@sasworkshops.com>
2025-05-06 14:57:43 +0000

    added raise method not called yet

--- player.py
+++ player.py
@@ -12,6 +12,14 @@ class Player:
 
         return current_buy_in - players[in_action]['bet']
 
+    @staticmethod
+    def raise(game_state, additional=0):
+        in_action = game_state['in_action']
+        current_buy_in = game_state['current_buy_in']
+        players = game_state['players']
+        minimum_raise = game_state['minimum_raise']
+        return current_buy_in - players[in_action]['bet'] + minimum_raise + additional
+
     def showdown(self, game_state):
         pass
 
