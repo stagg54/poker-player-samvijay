f495e731e8fbf4c571a847f5ef009d4e036c8de9
Samuel Taggart <sam@sasworkshops.com>
2025-05-06 15:02:45 +0000

    Revert "Refactored out call function"
    This reverts commit a7ab8b4680798235dfa42d4f90abee8921916235.

--- player.py
+++ player.py
@@ -2,13 +2,9 @@ class Player:
     VERSION = "call every time"
 
     def betRequest(self, game_state):
-        return self.call(game_state)
-
-    @staticmethod
-    def call(game_state):
-        in_action = game_state['in_action']
-        current_buy_in = game_state['current_buy_in']
-        players = game_state['players']
+        in_action=game_state['in_action']
+        current_buy_in=game_state['current_buy_in']
+        players=game_state['players']
 
         return current_buy_in - players[in_action]['bet']
 
