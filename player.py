a7ab8b4680798235dfa42d4f90abee8921916235
Samuel Taggart <sam@sasworkshops.com>
2025-05-06 14:55:11 +0000

    Refactored out call function

--- player.py
+++ player.py
@@ -2,9 +2,13 @@ class Player:
     VERSION = "call every time"
 
     def betRequest(self, game_state):
-        in_action=game_state['in_action']
-        current_buy_in=game_state['current_buy_in']
-        players=game_state['players']
+        return self.call(game_state)
+
+    @staticmethod
+    def call(game_state):
+        in_action = game_state['in_action']
+        current_buy_in = game_state['current_buy_in']
+        players = game_state['players']
 
         return current_buy_in - players[in_action]['bet']
 
