0272fcaf39475345777e87f80b5417e8a2a7b382
Samuel Taggart <sam@sasworkshops.com>
2025-05-06 15:11:39 +0000

    Added possible responses

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 class Player:
-    VERSION = "call every time"
+    VERSION = "call every time - added repsonses"
 
     def betRequest(self, game_state):
         return self.call(game_state)
@@ -10,6 +10,24 @@ class Player:
         current_buy_in = game_state['current_buy_in']
         players = game_state['players']
         return current_buy_in - players[in_action]['bet']
+
+    @staticmethod
+    def bet(game_state, additional=0):
+        in_action = game_state['in_action']
+        current_buy_in = game_state['current_buy_in']
+        players = game_state['players']
+        minimum_raise = game_state["minimum_raise"]
+        return current_buy_in - players[in_action]['bet'] + minimum_raise + additional
+
+    @staticmethod
+    def check():
+        return 0
+
+    @staticmethod
+    def all_in(game_state):
+        in_action = game_state['in_action']
+        players = game_state['players']
+        return players[in_action]['stack']
 
     def showdown(self, game_state):
         pass
