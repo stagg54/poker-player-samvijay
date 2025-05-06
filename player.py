6859c98e8c5de9e17101a61713ef5d4af0e069d4
Samuel Taggart <sam@sasworkshops.com>
2025-05-06 15:54:57 +0000

    added some print statements

--- player.py
+++ player.py
@@ -6,13 +6,18 @@ class Player:
         us = game_state['players'][in_action]
         high_cards = ["A","K","Q","J","10"]
         if game_state['round'] == 0:
+            print("Round one")
             if us["hole_cards"]["rank"][0] == us["hole_cards"]["rank"][1]:
+                print("pocket pair")
                 return self.all_in(game_state)
             if us["hole_cards"]["rank"][0] in high_cards or us["hole_cards"]["rank"][1] in high_cards:
+                print("high card")
                 return self.bet(game_state)
             else:
+                print("neither")
                 return self.check()
         else:
+            print("not first round calling")
             return self.call(game_state)
 
     @staticmethod
